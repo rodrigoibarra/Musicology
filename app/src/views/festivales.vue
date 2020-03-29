d78e455dac21d8ecdef6ab0833fed9d8416ba9bc
rodrigoibarra <hola@rodrigo-ibarra.com>
2020-03-29 12:17:43 -0600

    festival view is getting done

--- app/src/views/festivales.vue
+++ app/src/views/festivales.vue
@@ -1,22 +1,20 @@
 <template>
   <section class="musicShowList">
-        <h1 class="sectionTitle">Events I've been to.</h1>
-
+        <h1 class="sectionTitle">Some Festivals I've been to.</h1>
         <p class="error" v-if="error">{{ error }}</p>
-
-        <details class="eventItem" :class="event.tosClass"
-        v-for="(event, index) in eventsIsType"
-        v-bind:key="event.tosClass"
-        >
-            <summary class="summaryTitle">{{event.eventName}}</summary>
+        <div class="eventsGrid">
+          <details class="eventItem" :class="event.tosClass"
+        v-for="(event) in eventsIsType"
+        v-bind:key="event.tosClass">
+            <summary class="summaryTitle">{{event.eventName}} <time datetime:{{event.eventDate}}>{{event.eventDate}}</time></summary>
             <ul class="eventInfo">
                 <li class="eventCity"><span>City:</span>{{event.eventCity}}</li>
                 <li class="eventVenue"><span>Venue:</span> {{event.eventVenue}}</li>
-                <li class="eventDate"><span>Date:</span><time datetime="2015-09-26">{{event.eventDate}}</time></li>
+                <li class="eventDate"><span>Date:</span><time datetime:'{{event.eventDate}}'>{{event.eventDate}}</time></li>
             </ul>
             <p class="eventBarcode">{{event.eventName}}{{event.eventDate}}</p>
         </details>
-
+        </div>
     </section>
 </template>
 
